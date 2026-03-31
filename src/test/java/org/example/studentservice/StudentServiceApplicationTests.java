b11e422f36fb871e624452e681869c38c8ee3615
bishmie <bishmimalshi@gmail.com>
2026-03-31 14:54:24 +0530

    update yaml files

--- src/test/java/org/example/studentservice/StudentServiceApplicationTests.java
+++ src/test/java/org/example/studentservice/StudentServiceApplicationTests.java
@@ -1,6 +1,7 @@
 package org.example.studentservice;
 
 import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.autoconfigure.jdbc.AutoConfigureTestDatabase;
 import org.springframework.boot.test.context.SpringBootTest;
 
 @SpringBootTest
